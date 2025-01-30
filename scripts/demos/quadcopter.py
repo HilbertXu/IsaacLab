kellyguo11/IsaacLab-public

Renames Isaac Lab extensions and folders (#198)
This change renames Isaac Lab extensions and folders to simplify the
directory structure.

- source/extensions/omni.isaac.lab --> source/isaaclab
- source/extensions/omni.isaac.lab_assets --> source/isaaclab_assets
- source/extensions/omni.isaac.lab_tasks --> source/isaaclab_tasks
- source/apps --> apps
- source/standalone --> scripts
- source/standalone/workflows/rl_games -->
scripts/reinforcement_learning/rl_games
- source/standalone/workflows/rsl_rl -->
scripts/reinforcement_learning/rsl_rl
- source/standalone/workflows/skrl -->
scripts/reinforcement_learning/skrl
- source/standalone/workflows/sb3 --> scripts/reinforcement_learning/sb3
- source/standalone/workflows/ray --> scripts/reinforcement_learning/ray

<!-- As you go through the list, delete the ones that are not
applicable. -->

- New feature (non-breaking change which adds functionality)
- Breaking change (fix or feature that would cause existing
functionality to not work as expected)
- This change requires a documentation update

- [x] I have run the [`pre-commit` checks](https://pre-commit.com/) with
`./isaaclab.sh --format`
- [x] I have made corresponding changes to the documentation
- [x] My changes generate no new warnings
- [ ] I have added tests that prove my fix is effective or that my
feature works
- [x] I have updated the changelog and the corresponding version in the
extension's `config/extension.toml` file
- [ ] I have added my name to the `CONTRIBUTORS.md` or my name already
exists there

<!--
As you go through the checklist above, you can mark something as done by
putting an x character in it

For example,
- [x] I have done this task
- [ ] I have not done this task
-->

scripts/demos/quadcopter.py
@@ -1,0 +1,118 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+This script demonstrates how to simulate a quadcopter.
+
+.. code-block:: bash
+
+    # Usage
+    ./isaaclab.sh -p scripts/demos/quadcopter.py
+
+"""
+
+"""Launch Isaac Sim Simulator first."""
+
+import argparse
+import torch
+
+from isaaclab.app import AppLauncher
+
+# add argparse arguments
+parser = argparse.ArgumentParser(description="This script demonstrates how to simulate a quadcopter.")
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# parse the arguments
+args_cli = parser.parse_args()
+
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+"""Rest everything follows."""
+
+import isaaclab.sim as sim_utils
+from isaaclab.assets import Articulation
+from isaaclab.sim import SimulationContext
+
+##
+# Pre-defined configs
+##
+from isaaclab_assets import CRAZYFLIE_CFG  # isort:skip
+
+
+def main():
+    """Main function."""
+    # Load kit helper
+    sim_cfg = sim_utils.SimulationCfg(dt=0.005, device=args_cli.device)
+    sim = SimulationContext(sim_cfg)
+    # Set main camera
+    sim.set_camera_view(eye=[0.5, 0.5, 1.0], target=[0.0, 0.0, 0.5])
+
+    # Spawn things into stage
+    # Ground-plane
+    cfg = sim_utils.GroundPlaneCfg()
+    cfg.func("/World/defaultGroundPlane", cfg)
+    # Lights
+    cfg = sim_utils.DistantLightCfg(intensity=3000.0, color=(0.75, 0.75, 0.75))
+    cfg.func("/World/Light", cfg)
+
+    # Robots
+    robot_cfg = CRAZYFLIE_CFG.replace(prim_path="/World/Crazyflie")
+    robot_cfg.spawn.func("/World/Crazyflie", robot_cfg.spawn, translation=robot_cfg.init_state.pos)
+
+    # create handles for the robots
+    robot = Articulation(robot_cfg)
+
+    # Play the simulator
+    sim.reset()
+
+    # Fetch relevant parameters to make the quadcopter hover in place
+    prop_body_ids = robot.find_bodies("m.*_prop")[0]
+    robot_mass = robot.root_physx_view.get_masses().sum()
+    gravity = torch.tensor(sim.cfg.gravity, device=sim.device).norm()
+
+    # Now we are ready!
+    print("[INFO]: Setup complete...")
+
+    # Define simulation stepping
+    sim_dt = sim.get_physics_dt()
+    sim_time = 0.0
+    count = 0
+    # Simulate physics
+    while simulation_app.is_running():
+        # reset
+        if count % 2000 == 0:
+            # reset counters
+            sim_time = 0.0
+            count = 0
+            # reset dof state
+            joint_pos, joint_vel = robot.data.default_joint_pos, robot.data.default_joint_vel
+            robot.write_joint_state_to_sim(joint_pos, joint_vel)
+            robot.write_root_link_pose_to_sim(robot.data.default_root_state[:, :7])
+            robot.write_root_com_velocity_to_sim(robot.data.default_root_state[:, 7:])
+            robot.reset()
+            # reset command
+            print(">>>>>>>> Reset!")
+        # apply action to the robot (make the robot float in place)
+        forces = torch.zeros(robot.num_instances, 4, 3, device=sim.device)
+        torques = torch.zeros_like(forces)
+        forces[..., 2] = robot_mass * gravity / 4.0
+        robot.set_external_force_and_torque(forces, torques, body_ids=prop_body_ids)
+        robot.write_data_to_sim()
+        # perform step
+        sim.step()
+        # update sim-time
+        sim_time += sim_dt
+        count += 1
+        # update buffers
+        robot.update(sim_dt)
+
+
+if __name__ == "__main__":
+    # run the main function
+    main()
+    # close sim app
+    simulation_app.close()

source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/mdp/terminations.py
@@ -1,0 +1,52 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Common functions that can be used to activate certain terminations.
+
+The functions can be passed to the :class:`isaaclab.managers.TerminationTermCfg` object to enable
+the termination introduced by the function.
+"""
+
+from __future__ import annotations
+
+import torch
+from typing import TYPE_CHECKING
+
+from isaaclab.assets import RigidObject
+from isaaclab.managers import SceneEntityCfg
+
+if TYPE_CHECKING:
+    from isaaclab.envs import ManagerBasedRLEnv
+
+
+def terrain_out_of_bounds(
+    env: ManagerBasedRLEnv, asset_cfg: SceneEntityCfg = SceneEntityCfg("robot"), distance_buffer: float = 3.0
+) -> torch.Tensor:
+    """Terminate when the actor move too close to the edge of the terrain.
+
+    If the actor moves too close to the edge of the terrain, the termination is activated. The distance
+    to the edge of the terrain is calculated based on the size of the terrain and the distance buffer.
+    """
+    if env.scene.cfg.terrain.terrain_type == "plane":
+        return False  # we have infinite terrain because it is a plane
+    elif env.scene.cfg.terrain.terrain_type == "generator":
+        # obtain the size of the sub-terrains
+        terrain_gen_cfg = env.scene.terrain.cfg.terrain_generator
+        grid_width, grid_length = terrain_gen_cfg.size
+        n_rows, n_cols = terrain_gen_cfg.num_rows, terrain_gen_cfg.num_cols
+        border_width = terrain_gen_cfg.border_width
+        # compute the size of the map
+        map_width = n_rows * grid_width + 2 * border_width
+        map_height = n_cols * grid_length + 2 * border_width
+
+        # extract the used quantities (to enable type-hinting)
+        asset: RigidObject = env.scene[asset_cfg.name]
+
+        # check if the agent is out of bounds
+        x_out_of_bounds = torch.abs(asset.data.root_link_pos_w[:, 0]) > 0.5 * map_width - distance_buffer
+        y_out_of_bounds = torch.abs(asset.data.root_link_pos_w[:, 1]) > 0.5 * map_height - distance_buffer
+        return torch.logical_or(x_out_of_bounds, y_out_of_bounds)
+    else:
+        raise ValueError("Received unsupported terrain type, must be either 'plane' or 'generator'.")

source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/inhand/mdp/commands/orientation_command.py
@@ -1,0 +1,144 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Sub-module containing command generators for 3D orientation goals for objects."""
+
+from __future__ import annotations
+
+import torch
+from collections.abc import Sequence
+from typing import TYPE_CHECKING
+
+import isaaclab.utils.math as math_utils
+from isaaclab.assets import RigidObject
+from isaaclab.managers import CommandTerm
+from isaaclab.markers.visualization_markers import VisualizationMarkers
+
+if TYPE_CHECKING:
+    from isaaclab.envs import ManagerBasedRLEnv
+
+    from .commands_cfg import InHandReOrientationCommandCfg
+
+
+class InHandReOrientationCommand(CommandTerm):
+    """Command term that generates 3D pose commands for in-hand manipulation task.
+
+    This command term generates 3D orientation commands for the object. The orientation commands
+    are sampled uniformly from the 3D orientation space. The position commands are the default
+    root state of the object.
+
+    The constant position commands is to encourage that the object does not move during the task.
+    For instance, the object should not fall off the robot's palm.
+
+    Unlike typical command terms, where the goals are resampled based on time, this command term
+    does not resample the goals based on time. Instead, the goals are resampled when the object
+    reaches the goal orientation. The goal orientation is considered to be reached when the
+    orientation error is below a certain threshold.
+    """
+
+    cfg: InHandReOrientationCommandCfg
+    """Configuration for the command term."""
+
+    def __init__(self, cfg: InHandReOrientationCommandCfg, env: ManagerBasedRLEnv):
+        """Initialize the command term class.
+
+        Args:
+            cfg: The configuration parameters for the command term.
+            env: The environment object.
+        """
+        # initialize the base class
+        super().__init__(cfg, env)
+
+        # object
+        self.object: RigidObject = env.scene[cfg.asset_name]
+
+        # create buffers to store the command
+        # -- command: (x, y, z)
+        init_pos_offset = torch.tensor(cfg.init_pos_offset, dtype=torch.float, device=self.device)
+        self.pos_command_e = self.object.data.default_root_state[:, :3] + init_pos_offset
+        self.pos_command_w = self.pos_command_e + self._env.scene.env_origins
+        # -- orientation: (w, x, y, z)
+        self.quat_command_w = torch.zeros(self.num_envs, 4, device=self.device)
+        self.quat_command_w[:, 0] = 1.0  # set the scalar component to 1.0
+
+        # -- unit vectors
+        self._X_UNIT_VEC = torch.tensor([1.0, 0, 0], device=self.device).repeat((self.num_envs, 1))
+        self._Y_UNIT_VEC = torch.tensor([0, 1.0, 0], device=self.device).repeat((self.num_envs, 1))
+        self._Z_UNIT_VEC = torch.tensor([0, 0, 1.0], device=self.device).repeat((self.num_envs, 1))
+
+        # -- metrics
+        self.metrics["orientation_error"] = torch.zeros(self.num_envs, device=self.device)
+        self.metrics["position_error"] = torch.zeros(self.num_envs, device=self.device)
+        self.metrics["consecutive_success"] = torch.zeros(self.num_envs, device=self.device)
+
+    def __str__(self) -> str:
+        msg = "InHandManipulationCommandGenerator:\n"
+        msg += f"\tCommand dimension: {tuple(self.command.shape[1:])}\n"
+        return msg
+
+    """
+    Properties
+    """
+
+    @property
+    def command(self) -> torch.Tensor:
+        """The desired goal pose in the environment frame. Shape is (num_envs, 7)."""
+        return torch.cat((self.pos_command_e, self.quat_command_w), dim=-1)
+
+    """
+    Implementation specific functions.
+    """
+
+    def _update_metrics(self):
+        # logs data
+        # -- compute the orientation error
+        self.metrics["orientation_error"] = math_utils.quat_error_magnitude(
+            self.object.data.root_link_quat_w, self.quat_command_w
+        )
+        # -- compute the position error
+        self.metrics["position_error"] = torch.norm(self.object.data.root_link_pos_w - self.pos_command_w, dim=1)
+        # -- compute the number of consecutive successes
+        successes = self.metrics["orientation_error"] < self.cfg.orientation_success_threshold
+        self.metrics["consecutive_success"] += successes.float()
+
+    def _resample_command(self, env_ids: Sequence[int]):
+        # sample new orientation targets
+        rand_floats = 2.0 * torch.rand((len(env_ids), 2), device=self.device) - 1.0
+        # rotate randomly about x-axis and then y-axis
+        quat = math_utils.quat_mul(
+            math_utils.quat_from_angle_axis(rand_floats[:, 0] * torch.pi, self._X_UNIT_VEC[env_ids]),
+            math_utils.quat_from_angle_axis(rand_floats[:, 1] * torch.pi, self._Y_UNIT_VEC[env_ids]),
+        )
+        # make sure the quaternion real-part is always positive
+        self.quat_command_w[env_ids] = math_utils.quat_unique(quat) if self.cfg.make_quat_unique else quat
+
+    def _update_command(self):
+        # update the command if goal is reached
+        if self.cfg.update_goal_on_success:
+            # compute the goal resets
+            goal_resets = self.metrics["orientation_error"] < self.cfg.orientation_success_threshold
+            goal_reset_ids = goal_resets.nonzero(as_tuple=False).squeeze(-1)
+            # resample the goals
+            self._resample(goal_reset_ids)
+
+    def _set_debug_vis_impl(self, debug_vis: TYPE_CHECKING):
+        # set visibility of markers
+        # note: parent only deals with callbacks. not their visibility
+        if debug_vis:
+            # create markers if necessary for the first time
+            if not hasattr(self, "goal_pose_visualizer"):
+                self.goal_pose_visualizer = VisualizationMarkers(self.cfg.goal_pose_visualizer_cfg)
+            # set visibility
+            self.goal_pose_visualizer.set_visibility(True)
+        else:
+            if hasattr(self, "goal_pose_visualizer"):
+                self.goal_pose_visualizer.set_visibility(False)
+
+    def _debug_vis_callback(self, event):
+        # add an offset to the marker position to visualize the goal
+        marker_pos = self.pos_command_w + torch.tensor(self.cfg.marker_pos_offset, device=self.device)
+        marker_quat = self.quat_command_w
+        # visualize the goal marker
+        self.goal_pose_visualizer.visualize(translations=marker_pos, orientations=marker_quat)

HilbertXu/IsaacLab

scripts/demos/quadcopter.py
@@ -1,0 +1,118 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+This script demonstrates how to simulate a quadcopter.
+
+.. code-block:: bash
+
+    # Usage
+    ./isaaclab.sh -p scripts/demos/quadcopter.py
+
+"""
+
+"""Launch Isaac Sim Simulator first."""
+
+import argparse
+import torch
+
+from isaaclab.app import AppLauncher
+
+# add argparse arguments
+parser = argparse.ArgumentParser(description="This script demonstrates how to simulate a quadcopter.")
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# parse the arguments
+args_cli = parser.parse_args()
+
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+"""Rest everything follows."""
+
+import isaaclab.sim as sim_utils
+from isaaclab.assets import Articulation
+from isaaclab.sim import SimulationContext
+
+##
+# Pre-defined configs
+##
+from isaaclab_assets import CRAZYFLIE_CFG  # isort:skip
+
+
+def main():
+    """Main function."""
+    # Load kit helper
+    sim_cfg = sim_utils.SimulationCfg(dt=0.005, device=args_cli.device)
+    sim = SimulationContext(sim_cfg)
+    # Set main camera
+    sim.set_camera_view(eye=[0.5, 0.5, 1.0], target=[0.0, 0.0, 0.5])
+
+    # Spawn things into stage
+    # Ground-plane
+    cfg = sim_utils.GroundPlaneCfg()
+    cfg.func("/World/defaultGroundPlane", cfg)
+    # Lights
+    cfg = sim_utils.DistantLightCfg(intensity=3000.0, color=(0.75, 0.75, 0.75))
+    cfg.func("/World/Light", cfg)
+
+    # Robots
+    robot_cfg = CRAZYFLIE_CFG.replace(prim_path="/World/Crazyflie")
+    robot_cfg.spawn.func("/World/Crazyflie", robot_cfg.spawn, translation=robot_cfg.init_state.pos)
+
+    # create handles for the robots
+    robot = Articulation(robot_cfg)
+
+    # Play the simulator
+    sim.reset()
+
+    # Fetch relevant parameters to make the quadcopter hover in place
+    prop_body_ids = robot.find_bodies("m.*_prop")[0]
+    robot_mass = robot.root_physx_view.get_masses().sum()
+    gravity = torch.tensor(sim.cfg.gravity, device=sim.device).norm()
+
+    # Now we are ready!
+    print("[INFO]: Setup complete...")
+
+    # Define simulation stepping
+    sim_dt = sim.get_physics_dt()
+    sim_time = 0.0
+    count = 0
+    # Simulate physics
+    while simulation_app.is_running():
+        # reset
+        if count % 2000 == 0:
+            # reset counters
+            sim_time = 0.0
+            count = 0
+            # reset dof state
+            joint_pos, joint_vel = robot.data.default_joint_pos, robot.data.default_joint_vel
+            robot.write_joint_state_to_sim(joint_pos, joint_vel)
+            robot.write_root_pose_to_sim(robot.data.default_root_state[:, :7])
+            robot.write_root_velocity_to_sim(robot.data.default_root_state[:, 7:])
+            robot.reset()
+            # reset command
+            print(">>>>>>>> Reset!")
+        # apply action to the robot (make the robot float in place)
+        forces = torch.zeros(robot.num_instances, 4, 3, device=sim.device)
+        torques = torch.zeros_like(forces)
+        forces[..., 2] = robot_mass * gravity / 4.0
+        robot.set_external_force_and_torque(forces, torques, body_ids=prop_body_ids)
+        robot.write_data_to_sim()
+        # perform step
+        sim.step()
+        # update sim-time
+        sim_time += sim_dt
+        count += 1
+        # update buffers
+        robot.update(sim_dt)
+
+
+if __name__ == "__main__":
+    # run the main function
+    main()
+    # close sim app
+    simulation_app.close()

source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/mdp/terminations.py
@@ -1,0 +1,52 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Common functions that can be used to activate certain terminations.
+
+The functions can be passed to the :class:`isaaclab.managers.TerminationTermCfg` object to enable
+the termination introduced by the function.
+"""
+
+from __future__ import annotations
+
+import torch
+from typing import TYPE_CHECKING
+
+from isaaclab.assets import RigidObject
+from isaaclab.managers import SceneEntityCfg
+
+if TYPE_CHECKING:
+    from isaaclab.envs import ManagerBasedRLEnv
+
+
+def terrain_out_of_bounds(
+    env: ManagerBasedRLEnv, asset_cfg: SceneEntityCfg = SceneEntityCfg("robot"), distance_buffer: float = 3.0
+) -> torch.Tensor:
+    """Terminate when the actor move too close to the edge of the terrain.
+
+    If the actor moves too close to the edge of the terrain, the termination is activated. The distance
+    to the edge of the terrain is calculated based on the size of the terrain and the distance buffer.
+    """
+    if env.scene.cfg.terrain.terrain_type == "plane":
+        return False  # we have infinite terrain because it is a plane
+    elif env.scene.cfg.terrain.terrain_type == "generator":
+        # obtain the size of the sub-terrains
+        terrain_gen_cfg = env.scene.terrain.cfg.terrain_generator
+        grid_width, grid_length = terrain_gen_cfg.size
+        n_rows, n_cols = terrain_gen_cfg.num_rows, terrain_gen_cfg.num_cols
+        border_width = terrain_gen_cfg.border_width
+        # compute the size of the map
+        map_width = n_rows * grid_width + 2 * border_width
+        map_height = n_cols * grid_length + 2 * border_width
+
+        # extract the used quantities (to enable type-hinting)
+        asset: RigidObject = env.scene[asset_cfg.name]
+
+        # check if the agent is out of bounds
+        x_out_of_bounds = torch.abs(asset.data.root_pos_w[:, 0]) > 0.5 * map_width - distance_buffer
+        y_out_of_bounds = torch.abs(asset.data.root_pos_w[:, 1]) > 0.5 * map_height - distance_buffer
+        return torch.logical_or(x_out_of_bounds, y_out_of_bounds)
+    else:
+        raise ValueError("Received unsupported terrain type, must be either 'plane' or 'generator'.")

source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/inhand/mdp/commands/orientation_command.py
@@ -1,0 +1,144 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Sub-module containing command generators for 3D orientation goals for objects."""
+
+from __future__ import annotations
+
+import torch
+from collections.abc import Sequence
+from typing import TYPE_CHECKING
+
+import isaaclab.utils.math as math_utils
+from isaaclab.assets import RigidObject
+from isaaclab.managers import CommandTerm
+from isaaclab.markers.visualization_markers import VisualizationMarkers
+
+if TYPE_CHECKING:
+    from isaaclab.envs import ManagerBasedRLEnv
+
+    from .commands_cfg import InHandReOrientationCommandCfg
+
+
+class InHandReOrientationCommand(CommandTerm):
+    """Command term that generates 3D pose commands for in-hand manipulation task.
+
+    This command term generates 3D orientation commands for the object. The orientation commands
+    are sampled uniformly from the 3D orientation space. The position commands are the default
+    root state of the object.
+
+    The constant position commands is to encourage that the object does not move during the task.
+    For instance, the object should not fall off the robot's palm.
+
+    Unlike typical command terms, where the goals are resampled based on time, this command term
+    does not resample the goals based on time. Instead, the goals are resampled when the object
+    reaches the goal orientation. The goal orientation is considered to be reached when the
+    orientation error is below a certain threshold.
+    """
+
+    cfg: InHandReOrientationCommandCfg
+    """Configuration for the command term."""
+
+    def __init__(self, cfg: InHandReOrientationCommandCfg, env: ManagerBasedRLEnv):
+        """Initialize the command term class.
+
+        Args:
+            cfg: The configuration parameters for the command term.
+            env: The environment object.
+        """
+        # initialize the base class
+        super().__init__(cfg, env)
+
+        # object
+        self.object: RigidObject = env.scene[cfg.asset_name]
+
+        # create buffers to store the command
+        # -- command: (x, y, z)
+        init_pos_offset = torch.tensor(cfg.init_pos_offset, dtype=torch.float, device=self.device)
+        self.pos_command_e = self.object.data.default_root_state[:, :3] + init_pos_offset
+        self.pos_command_w = self.pos_command_e + self._env.scene.env_origins
+        # -- orientation: (w, x, y, z)
+        self.quat_command_w = torch.zeros(self.num_envs, 4, device=self.device)
+        self.quat_command_w[:, 0] = 1.0  # set the scalar component to 1.0
+
+        # -- unit vectors
+        self._X_UNIT_VEC = torch.tensor([1.0, 0, 0], device=self.device).repeat((self.num_envs, 1))
+        self._Y_UNIT_VEC = torch.tensor([0, 1.0, 0], device=self.device).repeat((self.num_envs, 1))
+        self._Z_UNIT_VEC = torch.tensor([0, 0, 1.0], device=self.device).repeat((self.num_envs, 1))
+
+        # -- metrics
+        self.metrics["orientation_error"] = torch.zeros(self.num_envs, device=self.device)
+        self.metrics["position_error"] = torch.zeros(self.num_envs, device=self.device)
+        self.metrics["consecutive_success"] = torch.zeros(self.num_envs, device=self.device)
+
+    def __str__(self) -> str:
+        msg = "InHandManipulationCommandGenerator:\n"
+        msg += f"\tCommand dimension: {tuple(self.command.shape[1:])}\n"
+        return msg
+
+    """
+    Properties
+    """
+
+    @property
+    def command(self) -> torch.Tensor:
+        """The desired goal pose in the environment frame. Shape is (num_envs, 7)."""
+        return torch.cat((self.pos_command_e, self.quat_command_w), dim=-1)
+
+    """
+    Implementation specific functions.
+    """
+
+    def _update_metrics(self):
+        # logs data
+        # -- compute the orientation error
+        self.metrics["orientation_error"] = math_utils.quat_error_magnitude(
+            self.object.data.root_quat_w, self.quat_command_w
+        )
+        # -- compute the position error
+        self.metrics["position_error"] = torch.norm(self.object.data.root_pos_w - self.pos_command_w, dim=1)
+        # -- compute the number of consecutive successes
+        successes = self.metrics["orientation_error"] < self.cfg.orientation_success_threshold
+        self.metrics["consecutive_success"] += successes.float()
+
+    def _resample_command(self, env_ids: Sequence[int]):
+        # sample new orientation targets
+        rand_floats = 2.0 * torch.rand((len(env_ids), 2), device=self.device) - 1.0
+        # rotate randomly about x-axis and then y-axis
+        quat = math_utils.quat_mul(
+            math_utils.quat_from_angle_axis(rand_floats[:, 0] * torch.pi, self._X_UNIT_VEC[env_ids]),
+            math_utils.quat_from_angle_axis(rand_floats[:, 1] * torch.pi, self._Y_UNIT_VEC[env_ids]),
+        )
+        # make sure the quaternion real-part is always positive
+        self.quat_command_w[env_ids] = math_utils.quat_unique(quat) if self.cfg.make_quat_unique else quat
+
+    def _update_command(self):
+        # update the command if goal is reached
+        if self.cfg.update_goal_on_success:
+            # compute the goal resets
+            goal_resets = self.metrics["orientation_error"] < self.cfg.orientation_success_threshold
+            goal_reset_ids = goal_resets.nonzero(as_tuple=False).squeeze(-1)
+            # resample the goals
+            self._resample(goal_reset_ids)
+
+    def _set_debug_vis_impl(self, debug_vis: TYPE_CHECKING):
+        # set visibility of markers
+        # note: parent only deals with callbacks. not their visibility
+        if debug_vis:
+            # create markers if necessary for the first time
+            if not hasattr(self, "goal_pose_visualizer"):
+                self.goal_pose_visualizer = VisualizationMarkers(self.cfg.goal_pose_visualizer_cfg)
+            # set visibility
+            self.goal_pose_visualizer.set_visibility(True)
+        else:
+            if hasattr(self, "goal_pose_visualizer"):
+                self.goal_pose_visualizer.set_visibility(False)
+
+    def _debug_vis_callback(self, event):
+        # add an offset to the marker position to visualize the goal
+        marker_pos = self.pos_command_w + torch.tensor(self.cfg.marker_pos_offset, device=self.device)
+        marker_quat = self.quat_command_w
+        # visualize the goal marker
+        self.goal_pose_visualizer.visualize(translations=marker_pos, orientations=marker_quat)