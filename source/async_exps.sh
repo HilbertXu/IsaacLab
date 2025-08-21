22390fecabd65ce7655d764ccbb8034e72bb3a04
HilbertXu <hilbertxu@outlook.com>
2025-08-21 18:54:44 +0100

    update experiment scripts

--- source/async_exps.sh
+++ source/async_exps.sh
@@ -3,33 +3,33 @@
 ################################################################
 # with reference, velocity control, with/without reaching stage
 ################################################################
-./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-wref-vel-wreach --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 4000
+./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-wref-vel-wreach --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 5000
 
-./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-wref-vel-woreach --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 4000
+./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-wref-vel-woreach --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 5000
 
 
 # ################################################################
 # # with reference, position control, with/without reaching stage
 # ################################################################
-./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-wref-pos-wreach --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 4000
+./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-wref-pos-wreach --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 5000
 
-./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-wref-pos-woreach --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 4000
+./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-wref-pos-woreach --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 5000
 
 
 ################################################################
 # without reference, velocity control, with/without reaching stage
 ################################################################
-./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-woref-vel-wreach --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 4000
+./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-woref-vel-wreach --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 5000
 
-./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-woref-vel-woreach --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 4000
+./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-woref-vel-woreach --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 5000
 
 
 ################################################################
 # without reference, position control, with/without reaching stage
 ################################################################
-./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-woref-pos-wreach --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 4000
+./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-woref-pos-wreach --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 5000
 
-./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-woref-pos-woreach --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 4000
+./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-woref-pos-woreach --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 5000
 
 ./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-wref-vel-wreach-nochunk --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 5000
 
@@ -40,4 +40,4 @@
 ./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-wref-vel-wreach --num_envs 6144 --reset_ratio 0.1 --seed 3407 --sweep --headless --max_iterations 5000
 
 
-# ./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-wref-vel-wreach-nochunk --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 4000
+# ./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task Async-Assembly-Baseline-wref-vel-wreach-nochunk --num_envs 6144 --reset_ratio 0.05 --seed 3407 --sweep --headless --max_iterations 5000
